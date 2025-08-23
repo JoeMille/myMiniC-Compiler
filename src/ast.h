6959fbd8b444f79ab91c8c06d88aa89375721e02
Joe M. <chefjoemiller1992@gmail.com>
2025-08-23 13:05:07 +0100

    Initial sketch of ast.h

--- src/ast.h
+++ src/ast.h
@@ -1,6 +1,35 @@
+// include this header just the once during compilation
 #pragma once
+
+
 #include <memory>
 #include <string>
 #include <vector>
 
+// base node gives a common ancestor useful for virtual destruction
 struct Node { virtual ~Node() = default; };
+
+// any copmutable value 
+struct Expr : Node {};
+
+// actions (ie return, if, while etc.,)
+struct Stmt : Node {};
+
+struct IntLiteral : Expr {
+    int value;
+};
+
+// return statements 
+struct ReturnStmt : Stmt {
+    std::unique_ptr<Expr> expr;
+};
+
+struct Function {
+    std:: string name;
+    std::unique_ptr<Stmt> body;
+};
+
+// program route for all top level functions
+struct Program {
+    std::vector<Function> functions;
+};
